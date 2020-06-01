30ec2d67a601674c087b940c2b1f57911e70abe0
zhongcai jiang <jiangzhongcai123@gmail.com>
2020-06-01 14:42:09 -0400

    q1-q5

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 console.log("hello")
-const container = document.getElementById(container)
-container
+ //q1
+const container = document.getElementById('container')
+ //q2
+const jqcontainer= document.querySelector("#container")
+//q3
+const second=document.getElementsByClassName('second')
+//q4
+container.getElementsByClassName('third')
+//q5
+contain=document.getElementById('container')
+contain.innerText="hello"
